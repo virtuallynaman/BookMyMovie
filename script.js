7ee94d50bff1149cf2ff1efcd88e5c23951def08
Naman Jha <jhanaman786@gmail.com>
2022-11-26 14:15:24 +0530

    adding local database

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
 const container = document.querySelector(".container");
-const selectedSeats = document.querySelector(".row .seat:not(.occupied)");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
 const count = document.getElementById("count");
 const total = document.getElementById("total");
-const movieSelect = document.getElementById("movie")
+const movieSelect = document.getElementById("movie");
 let ticketPrice = +movieSelect.value;
 
 //update count and total with price
-function updatePrice () {
+function updatePrice() {
     const selectedSeats = document.querySelectorAll(".row .seat.selected");
     const selectedSeatsCount = selectedSeats.length;
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+    localStorage.setItem("selectedSeats",JSON.stringify(seatsIndex));
 
     count.innerText = selectedSeatsCount;
     total.innerText = ticketPrice * selectedSeatsCount;
+}
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem("selectedMovieIndex", movieIndex);
+    localStorage.setItem("selectedMoviePrice", moviePrice)
 }
 
 //seat click event listener
@@ -25,5 +34,6 @@ container.addEventListener("click", e => {
 //movie selection event listener
 movieSelect.addEventListener("change", e => {
     ticketPrice = e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updatePrice();
 })
