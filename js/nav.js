326fabd78a01df81b21a1172569ffe825ef05548
Naman Jha <jhanaman786@gmail.com>
2022-11-27 21:55:52 +0530

    front page redisgned

---
+++ js/nav.js
@@ -0,0 +1,25 @@
+const createNav = () => {
+    let nav = document.querySelector(".navbar");
+
+    nav.innerHTML = `
+    <div class="nav">
+      <a href="index.html"><img src="images/logo.png" class="brand-logo" alt=""></a>
+      <div class="nav-items">
+          <div class="search">
+              <input type="text" class="search-box" placeholder="Search for Now Showing Movies and Upcoming Movies">
+              <button class="search-btn" onclick="window.location.href = 'search.html';">search</button>
+          </div>
+          <a href="signup.html"><img src="img/user.png" alt=""></a>
+          <a href="cart.html"><img src="img/cart.png" alt=""></a>
+      </div>
+  </div>
+  <ul class="links-container">
+      <li class="link-item"><a href="index.html" class="link">home</a></li>
+      <li class="link-item"><a href="#" class="link">men</a></li>
+      <li class="link-item"><a href="#" class="link">women</a></li>
+      <li class="link-item"><a href="#" class="link">shoes</a></li>
+  </ul>
+    `;
+}
+
+createNav();
